d4b1bd00f2fd391f56adbfab8c10b671995591cc
pridebj88 <pridebj88@gmail.com>
2020-06-22 17:51:04 +0900

    Atm 문제 풀이 추가(수정)

--- src/MeetingRoomAssignment.java
+++ src/MeetingRoomAssignment.java
@@ -1,35 +1,27 @@
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 /**
  * 문제 : https://www.acmicpc.net/problem/1931
  */
-public class MeetingRoomAssignment {
+public class MeetingRoomAssignment{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int numberOfMeeting = scanner.nextInt();
-        int[][] meetingTimeArray;
-        meetingTimeArray = new int[numberOfMeeting][2];
+        int[][] meetingTimeArray = new int[numberOfMeeting][2];
 
         for (int i = 0; i < numberOfMeeting; i++) {
             meetingTimeArray[i][0] = scanner.nextInt();
             meetingTimeArray[i][1] = scanner.nextInt();
         }
 
-        Arrays.sort(meetingTimeArray, (start, end) -> {
-            if (start[1] == end[1]) {
-                return Integer.compare(start[0], end[0]);
-            }
-            return Integer.compare(start[1], end[1]);
-        });
+        Arrays.sort(meetingTimeArray, (a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
 
         int count = 0;
-        int marking = 0;
+        int marking = -1;
 
         for (int i = 0; i < numberOfMeeting; i++) {
-            if (meetingTimeArray[i][0] > marking) {
+            if (meetingTimeArray[i][0] >= marking) {
                 marking = meetingTimeArray[i][1];
                 count++;
             }
