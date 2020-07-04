ee3469210c9fc14b4128f19221d2bc0ce6978d3d
pridebj88 <pridebj88@gmail.com>
2020-07-04 16:27:14 +0900

    기능개발 문제풀이 추가

--- src/FunctionDevelopement.java
+++ src/FunctionDevelopement.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * 문제 : 기능 개발
@@ -12,29 +10,45 @@ public class FunctionDevelopement {
     public static void main(String[] args) {
         int[] progresses = {93, 30, 55};
         int[] speeds = {1, 30, 5};
-        System.out.println("answer : {}" + solution(progresses, speeds));
+        int[] answer = solution(progresses, speeds);
+        System.out.println("answer : " + Arrays.toString(answer));
     }
 
     public static int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> answerList = new ArrayList<>();
 
-        for(int i = 0; i < progresses.length; i++){
-            double a = (double) (100-progresses[i])/speeds[i];
+        // 기능 개발이 끝나는 시점을 계산한다. '100-(현재 진행률)/속도'를 구한 뒤 올림.
+        for (int i = 0; i < progresses.length; i++) {
+            double a = (double) (100 - progresses[i]) / speeds[i];
             int deployDay = (int) Math.ceil(a);
-            list.add(deployDay);
+            queue.add(deployDay);
         }
 
-        for(int i = 0; i< list.size(); i++){
-            int count = 1;
-            if(list.get(i)>list.get(i+1)){
-                count++;
-            }else{
+        int standard = 0;
+        if (!queue.isEmpty()) {
+            standard = queue.poll();
+        }
+        int count = 1;
 
+        //맨 앞에서 하나씩 빼서 다음 값이 작으면 카운트롤 올리고 크면 배포 리스트에 넣는다.
+        while (!queue.isEmpty()) {
+            int next = queue.poll();
+
+            if (standard >= next) {
+                count += 1;
+            } else {
+                answerList.add(count);
+                count = 1;
+                standard = next;
             }
         }
+        answerList.add(count);
 
-
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
 
         return answer;
     }
