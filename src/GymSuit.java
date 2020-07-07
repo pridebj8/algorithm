addc34cc9c27c1609d259e1343c7780d9a8090a1
pridebj88 <pridebj88@gmail.com>
2020-07-07 21:00:55 +0900

    체육복 문제 추가

---
+++ src/GymSuit.java
@@ -0,0 +1,21 @@
+/**
+ * 문제 : 체육봅
+ * https://programmers.co.kr/learn/courses/30/lessons/42862
+ */
+public class GymSuit {
+    public static void main(String[] args) {
+        int n = 0;
+        int[] lost = {};
+        int[] reserve = {};
+
+        System.out.println("answer : " + solution(n, lost, reserve));
+    }
+
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+}
