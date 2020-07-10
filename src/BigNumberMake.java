8826ede981b956f0ecbe8a02d545d9e0147b6e5f
pridebj88 <pridebj88@gmail.com>
2020-07-10 22:12:39 +0900

    큰 수 만들기 문제 풀이 추가

--- src/BigNumberMake.java
+++ src/BigNumberMake.java
@@ -1,2 +1,32 @@
+import java.util.*;
+
+/**
+ * 문제 : 큰 수 만들기
+ * https://programmers.co.kr/learn/courses/30/lessons/42883
+ */
 public class BigNumberMake {
+
+    public static void main(String[] args) {
+        String number = "1924";
+        int k = 2;
+        System.out.println("answer : " + solution(number, k));
+    }
+
+    public static String solution(String number, int k) {
+        char[] result = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stack.get(i);
+        }
+
+        return new String(result);
+    }
 }
