91c63b48945db5064c62441ad7fad469d3df8508
pridebj88 <pridebj88@gmail.com>
2020-07-13 22:13:46 +0900

    쇠막대기 문제 추가

---
+++ src/IronBar.java
@@ -0,0 +1,15 @@
+/**
+ * 문제 : 쇠막대기 (스택/큐)
+ * https://programmers.co.kr/learn/courses/30/lessons/42585
+ */
+public class IronBar {
+    public static void main(String[] args) {
+        String arrangement = "()(((()())(())()))(())";
+        solution(arrangement);
+    }
+
+    public static int solution(String arrangement) {
+        int answer = 0;
+        return answer;
+    }
+}
