e3bfc1170513fd73de1232f87305d88d73ff5e29
pridebj88 <pridebj88@gmail.com>
2020-07-14 23:39:39 +0900

    주식가격 문제 추가

---
+++ src/StockPrice.java
@@ -0,0 +1,15 @@
+/**
+ * 문제 : 주식 가격 (큐/스택)
+ * https://programmers.co.kr/learn/courses/30/lessons/42584
+ */
+public class StockPrice {
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+        solution(prices);
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] answer = {};
+        return answer;
+    }
+}
