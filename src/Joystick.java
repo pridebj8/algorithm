3e77a1935fa7ceae44d4fdf2fe259565fe8bcbfb
pridebj88 <pridebj88@gmail.com>
2020-07-15 22:23:58 +0900

    조이스틱 문제 추가

---
+++ src/Joystick.java
@@ -0,0 +1,16 @@
+/**
+ * 문제 : 조이스틱 (탐욕법)
+ * https://programmers.co.kr/learn/courses/30/lessons/42860
+ */
+public class Joystick {
+
+    public static void main(String[] args) {
+        String name = "";
+        solution(name);
+    }
+
+    public static int solution(String name) {
+        int answer = 0;
+        return answer;
+    }
+}
