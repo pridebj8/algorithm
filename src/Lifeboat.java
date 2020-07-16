b77efd0dd821b3a802af73c1dc8e7d5c5fe76914
pridebj88 <pridebj88@gmail.com>
2020-07-16 23:53:49 +0900

    구명보트 문제 추가

---
+++ src/Lifeboat.java
@@ -0,0 +1,18 @@
+/**
+ * 문제 : 구명보트 (탐욕 알고리즘)
+ * https://programmers.co.kr/learn/courses/30/lessons/42885
+ */
+public class Lifeboat {
+
+    public static void main(String[] args) {
+        int[] people = {70,50,80,50};
+        int limit = 100;
+        solution(people, limit);
+    }
+
+    public  static  int solution(int[] people, int limit){
+        int answer = 0;
+
+        return answer;
+    }
+}
