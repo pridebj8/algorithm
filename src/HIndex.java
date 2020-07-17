e4e3b9b5bfe8a81ff08a34c510e94e07338f85d3
pridebj88 <pridebj88@gmail.com>
2020-07-17 19:23:35 +0900

    H-index 문제 추가

---
+++ src/HIndex.java
@@ -0,0 +1,16 @@
+/**
+ * 문제 : H-index (정렬)
+ * https://programmers.co.kr/learn/courses/30/lessons/42747
+ */
+public class HIndex {
+
+    public static void main(String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+        solution(citations);
+    }
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+        return answer;
+    }
+}
