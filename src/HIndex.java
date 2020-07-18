b58c81a235bf4d3935eaee7638040df5010d4fd7
pridebj88 <pridebj88@gmail.com>
2020-07-18 15:04:41 +0900

    H-index 문제 풀이 추가

--- src/HIndex.java
+++ src/HIndex.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 문제 : H-index (정렬)
  * https://programmers.co.kr/learn/courses/30/lessons/42747
@@ -6,11 +8,22 @@ public class HIndex {
 
     public static void main(String[] args) {
         int[] citations = {3, 0, 6, 1, 5};
-        solution(citations);
+        System.out.println("answer : " + solution(citations));
     }
 
     public static int solution(int[] citations) {
         int answer = 0;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            int hIndex = citations.length - i;
+
+            if (citations[i] >= hIndex) {
+                answer = hIndex;
+                break;
+            }
+        }
+
         return answer;
     }
 }
